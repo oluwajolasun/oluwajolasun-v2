dbb6035735fe5d4d1789d0503b71a280480f8a14
Oluwajolasun Jaiyesimi <jolasunjaiyesimi@gmail.com>
2025-11-24 21:05:00 -0700

    updates=: removed old irrelevant experience

--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -157,49 +157,6 @@ const ExperiencePage = () => {
             </div>
             <hr />
           </li>
-          <li>
-            <hr />
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-end mb-1">
-              <time className="font-mono italic">October 2018 – June 2021</time>
-              <div className="text-lg font-black">Software Engineer</div>
-              <div className="text-lg font-black">
-                Woodstock Electronics Limited
-              </div>
-              <ul className="list-disc space-y-2 pl-5">
-                <li>
-                  Contributed to both frontend and backend development to
-                  enhance platform functionality.
-                </li>
-                <li>
-                  Developed interactive user interfaces using React and other
-                  modern technologies.
-                </li>
-                <li>
-                  Implemented backend components, working with RESTful APIs and
-                  databases for seamless data exchange.
-                </li>
-                <li>
-                  Proactively stayed updated with industry trends and
-                  continuously enhanced technical skills.
-                </li>
-              </ul>
-            </div>
-            <hr />
-          </li>
         </ul>
       </div>
     </div>
